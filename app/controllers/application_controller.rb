3e8685e6b3e2fee0c0eff2af46ba03fd023466a4
Bruce Li <pengyuli@chicagobooth.edu>
2019-03-24 03:14:16 +0000

    on section 1.4.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, raise: false
+  
+  def hello
+    render html: "¡hola, mundo!"
+  end
+  
+  def goodbye
+    render html: '¡adios, mundo!'
+  end
+  
 end
